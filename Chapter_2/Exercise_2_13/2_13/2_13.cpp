cd77a417e9b9acca915dc56e503401766f35a51d
Ben <fuzzyllama@gmail.com>
2013-10-09 23:14:09 -0700

    add 2_13

--- Chapter_2/Exercise_2_13/2_13/2_13.cpp
+++ Chapter_2/Exercise_2_13/2_13/2_13.cpp
@@ -1,37 +1,50 @@
 /*
- * File: 2_12.cpp
+ * File: 2_13.cpp
  * -------------
- * Half Life 3?
+ * Aproximate PI
  * 
- * This program simulates 10,000 atom of radioactive material decaying.
- *
+ * This program simulates 10,000 darts being thrown at a circle in a square.
+ * The dart lies inside the circle if x^2 + y^2 < 1
+ * The result should be PI/4
  */
 
 #include <iostream>
 #include "random.h"
-#include "simpio.h"
+
 using namespace std;
 
-double const DECAY_RATE = 0.5;
+int const DARTS_TROWN = 10000;
+
 /* Function Prototypes */
+void throwDarts(int & in);
+void printResults(double ratio, int in);
 
 int main() {
-    int atoms = 10000;
-    int year = 0;
-    cout << "There are " << atoms << " atoms initially." << endl;
+    int dartsInside = 0;
+    double ratio = 0.0;
     
-    while (atoms > 0) {
-        int currentDecay = atoms;
-        for (int i = 0; i < atoms; i++) {
-            if (randomChance(DECAY_RATE)) {
-                currentDecay--;
-            }
-        }
-        atoms = currentDecay;
-        year++;
-        cout << "There are " << atoms << " atoms at the end of year " << year << endl;
-    }
+    throwDarts(dartsInside);
+    ratio = double(dartsInside) / DARTS_TROWN;
+    printResults(ratio, dartsInside);
     
     return 0;
 }
 
+
+void throwDarts(int & in) {
+    double x, y;
+    
+    for (int i = 0; i < DARTS_TROWN; i++) {
+        x = randomReal(-1, 1);
+        y = randomReal(-1, 1);
+        
+        if ((x * x) + (y * y) < 1) in++;
+    }
+}
+
+void printResults(double ratio, int in) {
+    cout << DARTS_TROWN << " darts reveal:" << endl;
+    cout << in << " fell inside the circle" << endl;
+    cout << "creating a ratio of " << ratio << endl;
+    cout << "multiply by 4 to get the PI approximation of: " << ratio * 4.0;
+}
