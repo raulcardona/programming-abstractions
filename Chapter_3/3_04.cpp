b2d391cc6e3cfb8b4b95712fe5f782927723bd33
Ben Mills <fuzzyllama@gmail.com>
2013-10-12 13:36:26 -0700

    Create 3_04.cpp
    just started

---
+++ Chapter_3/3_04.cpp
@@ -0,0 +1,28 @@
+*/  File: 03_04.cpp
+*   --------------------------
+*   This program will implement a function
+*   capitalize(str) where the input string will 
+*   be returned with the first letter capitalized.
+*/
+
+#include <iostream>
+#include <string>
+#include "simpio.h"
+
+/* Function Prototypes */
+string capitalize(string str);
+
+int main() {
+
+return 0;
+}
+
+string capitalize(string str) {
+  int length = str.length();
+  for (int i = 0; i < length; i++) {
+    if (str.at(i) == 0) {
+        str.at(i) = str.at(i - )
+      }
+  
+  }
+}
