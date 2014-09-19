80868fd327271dfa85bc1ec9b9b5453fc6b20895
Ben <fuzzyllama@gmail.com>
2014-09-19 01:23:52 -0700

    finish 8_05

--- Chapter_08/Exercise_05/8_05.cpp
+++ Chapter_08/Exercise_05/8_05.cpp
@@ -5,44 +5,67 @@
 #include "vector.h"
 #include "lexicon.h"
 #include "simpio.h"
-#include "set.h"
 #include "foreach.h"
 using namespace std;
 
-Vector<string> fillSubsetVector(string str, string target);
-void printSubsetWords(Vector<string> vec);
+void fillVector(Vector<char> &vec, string str);
+bool hasWord(Vector<char> vec, string str);
+string eraseChar(string str, char ch);
+bool leftToRightOk(string line, string str);
 
-Vector<string> vec;
 
 int main() {
-	Lexicon dictionary("dictionary2.txt"); 
+	Lexicon dictionary("dictionary.txt"); 
 	string line = getLine("Enter starting word: ");
 	cout << "The embedded starting words are: " << endl;
+	Vector<char> vec;
+	fillVector(vec, line);
 	
-	
-
-	for (string word : dictionary) {
-		vec = fillSubsetVector(word, line);
-	}
-
-	printSubsetWords(vec);
-	return 0;
-}
-
-Vector<string> fillSubsetVector(string str, string target) {
-	if (str == "") {
-		return vec += str;
-	} else {
-		string rest = str.substr(1);
-		return vec += (fillSubsetVector(rest, target) + fillSubsetVector(rest, target.substr(1)));
-	}
-}
-
-void printSubsetWords(Vector<string> vec) {
-	for (string str : vec) {
-		if (str != "") {
+	for (string str : dictionary) {
+		if (hasWord(vec, str)) {
 			cout << str << endl;
 		}
 	}
+
+	return 0;
 }
 
+void fillVector(Vector<char> &vec, string str) {
+	for (int i = 0; i < str.length(); i++) {
+		vec.add(str[i]);
+	}
+}
+
+bool hasWord(Vector<char> vec, string str) {
+	if (vec.isEmpty()) {
+		return str == "";
+	} else {
+		char element = vec[0];
+		Vector<char> rest = vec;
+		rest.remove(0);
+		return hasWord(rest, str) || hasWord(rest, eraseChar(str, element));
+
+	}
+}
+
+string eraseChar(string str, char ch) {
+	string result = str;
+	if (ch == str[0]) {
+		result = result.erase(0, 1);
+		return result;
+	} else {
+		return result;
+	}
+	
+}
+
+/*	bool subsetSumExists(Set<int> &set, int target) {
+		if (set.isEmpty() {
+			return target == 0;
+		} else {
+			int element = set.first();
+			Set<int> rest = set - element;
+			return subsetSumExists(rest, target) || subsetSumExists(rest, target - element);
+		}
+	}
+*/
